5dc346ee8c027d67f43dbc07721059b64b4b19df
Katarzyna Dudziak <katarzynadudziak94@gmail.com>
2021-08-12 15:40:56 +0200

    Entering numbers by the player and error handling

--- main.py
+++ main.py
@@ -39,21 +39,47 @@ def is_level_correct(configs, player):
     return True
 
 
+def enter_the_numbers(config: Config):
+    print(f"Enter your {config.playerNumbers} unique numbers from "
+            f"{config.startOfRange} to {config.endOfRange} separated by a comma:")
+    selectedNumbers = input()
+    setSelectedNumbers = set()
+
+    for number in selectedNumbers.split(","):
+        try:
+            setSelectedNumbers.add(int(number))
+        except ValueError:
+            print("ERROR: numbers must be separated by one comma!")
+            exit()
+    return setSelectedNumbers
+
+
+def validating_numbers(setSelectedNumbers, config: Config):
+    if len(setSelectedNumbers) != config.playerNumbers:
+        
+        return False
+    return True
+   
+
 def main():
     player = Player()
     configs = [
         Config(1, 10, 3, "easy", 10),
         Config(1, 50, 5, "medium", 20),
-        Config(1, 100, 8, "hard", 40)
-    ]
+        Config(1, 100, 8, "hard", 40)]
     player.name = enter_your_name()
     player.level = choose_level(player, configs)
 
     if is_level_correct(configs, player):
-        print(f"{player.name} You selected level: {player.level}")
+        print(f"{player.name} you selected level: {player.level}")
+        enterNumbers = enter_the_numbers(configs[player.level - 1])
     else:
         print(f"{player.name} the selected level is incorrect!")
 
+    if validating_numbers(enterNumbers, configs[player.level - 1]) is False:
+        print(f"You entered the wrong number of numbers")
+    else:
+        print("Let's start the draw!")
 
 if __name__ == '__main__':
     main()
