45a0b23e8f9b5ecc6f49185e4ffa64a2f7ceb27f
Katarzyna Dudziak <katarzynadudziak94@gmail.com>
2021-10-17 16:18:50 +0200

    Tests merged to parametrize

--- tests.py
+++ tests.py
@@ -4,13 +4,10 @@ from main import *
 import pytest
 
 
-
-
 configs = [
         Config(1, 10, 3, "easy", 5),
         Config(1, 50, 5, "medium", 10),
         Config(1, 100, 8, "hard", 15)]
-
 
 
 @pytest.fixture
@@ -49,26 +46,20 @@ def game_time(mocker):
     return time
 
 
-class TestPlayer():
+class TestPlayer:
 
     def test_create_player_object(self, player_input):
         player_input.return_value = "asd"
 
         Player()
         
-    def test_input__name(self, player_input):
-        player_input.return_value = "tomek"
+    @pytest.mark.parametrize("test_input,expected", [("tomek", "tomek"), ("", "player")])
+    def test_input__name(self, player_input, test_input, expected):
+        player_input.return_value = test_input
 
         player = Player()
 
-        assert player.get_name() == "tomek"
-
-    def test_input_invalid_name(self, player_input):
-        player_input.return_value = ""
-
-        player = Player()
-
-        assert player.get_name() == "player"
+        assert player.get_name() == expected
 
     def test_add_points(self, player_input, player_time):
         player_input.return_value = ""
@@ -82,88 +73,56 @@ class TestPlayer():
         result = player.add_points(3)
         assert result == 8
 
-class TestGame():
+class TestGame:
 
     def test_create_game_object(self):
         Game()
-
-    def test_get_valid_config_from_user(self, game_input):
-        game_input.return_value = "1"
-
-        game = Game()
-
-        result = game.get_config_from_user()
-        assert result == 0
-        
-    def test_get_invalid_config_from_user(self, game_input):
-        game_input.side_effect = ["w", ""]
-
-        game = Game()
-
-        result = game.get_config_from_user()
-        assert result == -1
-        
-    def test_choose_valid_config(self, game_input, player_mock):
-        game_input.return_value = "1"
-        player_mock.get_name.return_value = "player"
-
-        game = Game()
-        game.choose_config(configs, player_mock)
-
-        assert player_mock.get_name.call_count == 1
-        
-    def test_choose_invalid_config(self, game_input, player_mock):
-        game_input.side_effect = [4,2]
-        player_mock.get_name.return_value = "player"
-
-        game = Game()
-        game.choose_config(configs, player_mock)
-
-        assert player_mock.get_name.call_count == 2
     
-    def test_get_valid_numbers(self, game_input, player_mock):
-        game_input.side_effect = ["1", "1,2,3"]
+    @pytest.mark.parametrize("test_input, expected", [("w", -1), ("", -1), ("1", 0)])
+    def test_get_config_from_user(self, game_input, test_input, expected):
+        game_input.return_value = test_input
+
+        game = Game()
+
+        assert game.get_config_from_user() == expected
+        
+    @pytest.mark.parametrize("test_input, expected", [(["1"], 1), (["4", "2"], 2)])
+    def test_choose_config(self, game_input, player_mock, test_input, expected):
+        game_input.side_effect = test_input
         player_mock.get_name.return_value = "player"
 
         game = Game()
         game.choose_config(configs, player_mock)
 
-        result = game.get_numbers()
-        assert result == {1,2,3}
-    
-    def test_get_invalid_character(self, game_input, player_mock):
-        game_input.side_effect = ["1", "a,cd,,", "1,2,3"]
+        assert player_mock.get_name.call_count == expected
+ 
+    @pytest.mark.parametrize("test_input, expected", [
+        (["1", "1,2,3"], {1,2,3}),
+        (["1", "a,b,.", "3,2,5"], {3,2,5})
+    ])
+    def test_get_numbers(self, game_input, player_mock, test_input, expected):
+        game_input.side_effect = test_input
         player_mock.get_name.return_value = "player"
 
         game = Game()
         game.choose_config(configs, player_mock)
 
-        result = game.get_numbers()
-        assert result == {1,2,3}
+        assert game.get_numbers() == expected
 
-    def test_is_nubers_valid(self, game_input, player_mock):
+    @pytest.mark.parametrize("numbers, expected", [
+        ({1,2,3}, True),
+        ({1,2,3,4,5}, False),
+        ({1,2}, False),
+        ({11,22,33}, False)
+    ])
+    def test_is_numbers_valid(self, game_input, player_mock, numbers, expected):
         game_input.return_value = "1"
         player_mock.get_name.return_value = "player"
 
         game = Game()
         game.choose_config(configs, player_mock)
 
-        result = game.is_numbers_valid({1,2,3})
-        assert result == True
-    
-    def test_is_nubers_invalid(self, game_input, player_mock):
-        game_input.return_value = "1"
-        player_mock.get_name.return_value = "player"
-
-        game = Game()
-        game.choose_config(configs, player_mock)
-
-        result = game.is_numbers_valid({1,2,3,4,5})
-        assert result == False
-        result = game.is_numbers_valid({1,2})
-        assert result == False
-        result = game.is_numbers_valid({11,22,33})
-        assert result == False
+        assert game.is_numbers_valid(numbers) == expected
     
     def test_draw_numbers(self, game_input, player_mock, randint_mock):
         game_input.return_value = "1"
@@ -175,28 +134,26 @@ class TestGame():
         
         result = game.draw_numbers()
         assert result == {1,2,3}
-        
-    def test_find_hit_numbers(self, game_time):
+    
+    @pytest.mark.parametrize("choose_numbers, drawn_numbers, hit_numbers", [
+        ({1,2,3}, {1,2,3}, {1,2,3}),
+        ({6,4,8},{1,2,3}, set()),
+        ({1,6,8}, {1,2,3}, {1})
+    ])
+    def test_find_hit_numbers(self, game_time, choose_numbers, drawn_numbers, hit_numbers):
         game = Game()
 
-        result = game.find_hit_numbers({1,2,3}, {1,2,3})
-        assert result == {1,2,3}
-        result = game.find_hit_numbers({6,4,8}, {1,2,3})
-        assert result == set()
-        result = game.find_hit_numbers({1,6,8}, {1,2,3})
-        assert result == {1}
+        assert game.find_hit_numbers(choose_numbers, drawn_numbers) == hit_numbers
 
-    def test_calculate_points(self, game_input, player_mock):
+    @pytest.mark.parametrize("hit_numbers, points", [({1,2}, 10), ({}, 0)])
+    def test_calculate_points(self, game_input, player_mock, hit_numbers, points):
         game_input.return_value = "1"
         player_mock.get_name.return_value = "player"
 
         game = Game()
         game.choose_config(configs, player_mock)
 
-        result = game.calculate_points({1,2})
-        assert result == 10
-        result = game.calculate_points({})
-        assert result == 0
+        assert game.calculate_points(hit_numbers) == points
 
     def test_play_again(self, game_input):
         game_input.side_effect = ["Y", "N", ""]
